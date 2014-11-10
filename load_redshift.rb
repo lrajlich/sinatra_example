e8aaa313a5830d4053be86a0d4113ad0f7dbcfa7
Luke Rajlich <lrajlich@gmail.com>
2014-11-10 14:51:11 -0800

    convert over failures to exceptions

--- load_redshift.rb
+++ load_redshift.rb
@@ -1,34 +1,18 @@
 require 'redtrack'
 require 'yaml'
-require './configuration'
+require './p_configuration'
 
 loader = $redtrack_client.new_loader()
 
 redshift_table = 'test_events'
 stream_shard_index = 0
 
-loader_result = loader.load_redshift_from_broker(redshift_table,stream_shard_index)
-
-if loader_result[:success] == false
-
-  information = {
-      :loader_result => loader_result,
-      :parameters => {
-          :redshift_table => redshift_table,
-          :stream_shard_index => stream_shard_index
-      },
-
-  }
-
-  puts YAML::dump(information)
-
-  if information[:failure_type] == "ERROR"
-    puts 'Load redshift Failed!'
-    exit 1
-  else
-    puts 'Load redshift Exited with warning!'
-    exit 0
-  end
+begin
+  loader_result = loader.load_redshift_from_broker(redshift_table,stream_shard_index)
+  puts 'Load redshift Succeeded.'
+  exit 0
+rescue RedTrack::LoaderException => e
+  puts YAML.dump(e.information)
+  puts e.message
+  exit 1
 end
-
-puts 'Load redshift Succeeded.'
